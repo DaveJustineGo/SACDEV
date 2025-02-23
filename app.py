a6d02412a5fb1136b5f4823b7a4f777a5e358bbf
Feary Mylls Yumo <20180013279@my.xu.edu.ph>
2025-02-24 07:48:03 +0800

    flask 2

--- app.py
+++ app.py
@@ -1,8 +1,35 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://username:password@localhost/xu_sacdev'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-students[] = []
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# Organization Model
+class Organization(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), nullable=False)
+    president = db.Column(db.String(255), nullable=False)
+    constitution = db.Column(db.Text, nullable=True)
+
+# Student Leader Model
+class StudentLeader(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), nullable=False)
+    position = db.Column(db.String(255), nullable=False)
+    organization_id = db.Column(db.Integer, db.ForeignKey('organization.id'), nullable=False)
+
+# Student Document Model
+class StudentDocument(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), nullable=False)
+    file_type = db.Column(db.String(255), nullable=False)
+    data = db.Column(db.LargeBinary, nullable=False)
+
 
 @app.route('/')
 def home():
